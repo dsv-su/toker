bf5eb5a600345cdc3e1d8096a735739a17413e20
Andreas Svanberg <andreass@dsv.su.se>
2022-12-23 12:46:01 +0100

    Allow verifying tokens via GET using bearer authorization header

--- core/src/main/scala/se/su/dsv/oauth/endpoint/Verify.scala
+++ core/src/main/scala/se/su/dsv/oauth/endpoint/Verify.scala
@@ -8,6 +8,8 @@ import cats.syntax.all._
 import org.http4s._
 import org.http4s.argonaut._
 import org.http4s.dsl._
+import org.http4s.headers.Authorization
+import org.http4s.util.CaseInsensitiveString
 import se.su.dsv.oauth.{Payload, Token}
 
 class Verify[F[_]]
@@ -17,6 +19,15 @@ class Verify[F[_]]
 {
 
   def service: HttpRoutes[F] = HttpRoutes.of[F] {
+    case request @ GET -> Root =>
+      request.headers.get(Authorization) match {
+        case Some(Authorization(Credentials.Token(scheme, token))) if scheme == CaseInsensitiveString("Bearer") =>
+          lookupToken(Token(token)).value.flatMap {
+            case Some(payload) => Ok(payload.asJson)
+            case None => Forbidden()
+          }
+        case _ => Forbidden()
+      }
     case request @ POST -> Root =>
       val prg = for {
         uuid <- OptionT.liftF(request.as[String])
