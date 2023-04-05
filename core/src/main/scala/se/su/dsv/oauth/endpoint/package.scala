18284e85ea72fd4ef11a4687378c13a4adccdfb9
Andreas Svanberg <andreass@dsv.su.se>
2023-04-05 12:43:00 +0200

    Conform to error response format

--- core/src/main/scala/se/su/dsv/oauth/endpoint/package.scala
+++ core/src/main/scala/se/su/dsv/oauth/endpoint/package.scala
@@ -1,6 +1,9 @@
 package se.su.dsv.oauth
 
-import org.http4s.{HttpVersion, MessageFailure, Response, Status}
+import io.circe.Json
+import org.http4s.{Challenge, HttpVersion, MessageFailure, Response, Status}
+import org.http4s.circe._
+import org.http4s.headers.`WWW-Authenticate`
 
 package object endpoint {
   object InvalidCredentials extends MessageFailure {
@@ -10,5 +13,7 @@ package object endpoint {
 
     override def toHttpResponse[F[_]](httpVersion: HttpVersion): Response[F] =
       Response(Status.Unauthorized, httpVersion)
+        .withHeaders(`WWW-Authenticate`(Challenge("Basic", "toker")))
+        .withEntity(Json.obj("error" -> Json.fromString("invalid_client")))
   }
 }
